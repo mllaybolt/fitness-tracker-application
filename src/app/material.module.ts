7b80fd4fe35684b1cf9c962d24a3b9f2bcd8a2b8
Michelle <mllaybolt@gmail.com>
2021-09-14 09:02:28 -0500

    Added date picker and date restrictions for age

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,9 +3,25 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  imports: [MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
-  exports: [MatIconModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
 export class MaterialModule {}
