543311321efe8e1d44d146e75d9e88c2c6545899
Michelle <mllaybolt@gmail.com>
2021-09-14 13:26:38 -0500

    Removed age restriction for time being due to bug in tsconfig.json file

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -7,13 +7,13 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  maxDate;
+  // maxDate;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.maxDate = new Date();
-    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+    // this.maxDate = new Date();
+    // this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   onSubmit(form: NgForm) {
