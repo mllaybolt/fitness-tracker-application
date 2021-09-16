52487e89f2bc18d803a799b3801410f5d0e509f4
Michelle <mllaybolt@gmail.com>
2021-09-16 16:32:03 -0500

    Added route protection

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -32,6 +32,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.sidenavToggle.emit();
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.authSubscription.unsubscribe();
   }
